c29624eb795ba5e976a9e2471316986c2fac91fb
john-ext <mucunga90@gmail.com>
2022-09-28 15:05:43 +0300

    fix ssl path issue

--- server/server.go
+++ server/server.go
@@ -126,8 +126,8 @@ func (m *Meta) serve() {
 	}
 
 	// create server connection
-	crt := os.Getenv("APP_PATH") + "ssl" + "/server.crt"
-	key := os.Getenv("APP_PATH") + "ssl" + "/server.key"
+	crt := os.Getenv("APP_PATH") + "/ssl/server.crt"
+	key := os.Getenv("APP_PATH") + "/ssl/server.key"
 
 	// generate self-sing key
 	err = GenerateSelfSignedCert(os.Getenv("SERVER_HOST"), crt, key)
