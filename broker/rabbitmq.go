3e4920ea9d1a3ce8f9b4d16bde0e78156b8fa320
john-ext <mucunga90@gmail.com>
2022-10-17 19:51:54 +0300

    fix rmq event lisstener

--- broker/rabbitmq.go
+++ broker/rabbitmq.go
@@ -7,8 +7,6 @@ import (
 	amqp "github.com/greatfocus/gf-amqp"
 )
 
-var appId = "a6ced560-ea7a-4693-b990-7aada41982bf"
-
 // Rabbitmq -
 type Rabbitmq struct {
 	conn *amqp.Connection
@@ -35,61 +33,25 @@ func GetConn() (*Rabbitmq, error) {
 }
 
 // Publish -
-func Publish(data []byte) error {
+func Publish(queueName, routingKey, messageId string, data []byte) error {
 	// get connection
+	var appId = os.Getenv("APP_ID")
 	conn, err := GetConn()
 	if err != nil {
 		return err
 	}
 
-	err = conn.ch.ExchangeDeclare(
-		appId,
-		"fanout",
-		true,
-		false,
-		false,
-		false,
-		nil,
+	conn.err = conn.ch.ExchangeDeclare(
+		appId,   // name
+		"topic", // type
+		true,    // durable
+		false,   // auto-deleted
+		false,   // internal
+		false,   // no-wait
+		nil,     // arguments
 	)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Broker published a massage: %s", data)
-	return conn.ch.Publish(
-		// exchange - yours may be different
-		appId,
-		appId,
-		// mandatory - we don't care if there I no queue
-		false,
-		// immediate - we don't care if there is no consumer on the queue
-		false,
-		amqp.Publishing{
-			ContentType:  "application/json",
-			Body:         data,
-			DeliveryMode: amqp.Persistent,
-		})
-}
-
-// StartConsumer -
-func Consumer(queueName string, handler func(d amqp.Delivery) bool, concurrency int) error {
-	// get connection
-	conn, err := GetConn()
-	if err != nil {
-		return err
-	}
-
-	err = conn.ch.ExchangeDeclare(
-		appId,
-		"fanout",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		return err
+	if conn.err != nil {
+		return conn.err
 	}
 
 	// create the queue if it doesn't already exist
@@ -99,13 +61,42 @@ func Consumer(queueName string, handler func(d amqp.Delivery) bool, concurrency 
 	}
 
 	// bind the queue to the routing key
-	err = conn.ch.QueueBind(queueName, appId, appId, false, nil)
+	err = conn.ch.QueueBind(queueName, routingKey, appId, false, nil)
 	if err != nil {
 		return err
 	}
 
-	// prefetch 4x as many messages as we can handle at once
-	prefetchCount := concurrency * 4
+	log.Printf("Broker published a message: %s", data)
+	err = conn.ch.Publish(
+		appId,      // exchange
+		routingKey, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			AppId:        appId,
+			MessageId:    messageId,
+			ContentType:  "application/json",
+			Body:         data,
+			DeliveryMode: amqp.Persistent,
+		})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// StartConsumer -
+func Consumer(queueName, routingKey string, handler func(d amqp.Delivery) bool, concurrency int) error {
+	// get connection
+	var appId = os.Getenv("APP_ID")
+	conn, err := GetConn()
+	if err != nil {
+		return err
+	}
+
+	// prefetch 10x as many messages as we can handle at once
+	prefetchCount := concurrency * 10
 	err = conn.ch.Qos(prefetchCount, 0, false)
 	if err != nil {
 		return err
@@ -114,7 +105,7 @@ func Consumer(queueName string, handler func(d amqp.Delivery) bool, concurrency 
 	msgs, err := conn.ch.Consume(
 		queueName, // queue
 		"",        // consumer
-		true,      // auto-ack
+		false,     // auto-ack
 		false,     // exclusive
 		false,     // no-local
 		false,     // no-wait
@@ -126,18 +117,20 @@ func Consumer(queueName string, handler func(d amqp.Delivery) bool, concurrency 
 
 	// create a goroutine for the number of concurrent threads requested
 	for i := 0; i < concurrency; i++ {
-		log.Printf("Processing messages on thread %v...\n", i)
 		go func() {
 			for msg := range msgs {
 				// if tha handler returns true then ACK, else NACK
 				// the message back into the rabbit queue for
 				// another round of processing
-				log.Printf("Broker received a massage: %s", msg.Body)
-				if handler(msg) {
-					_ = msg.Ack(false)
-				} else {
-					_ = msg.Nack(false, true)
+				if msg.AppId == appId && msg.RoutingKey == routingKey {
+					log.Printf("Broker consumed a message: %s", msg.Body)
+					if handler(msg) {
+						_ = msg.Ack(false)
+					} else {
+						_ = msg.Nack(false, true)
+					}
 				}
+				_ = msg.Nack(false, true)
 			}
 		}()
 	}
