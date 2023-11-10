69a75d725fd640b8a2542d3d4dda0ff8c71dacc4
mucunga90 <mucunga90@gmail.com>
2023-11-10 22:06:21 +0300

    fix postgres sssl logic

--- server/server.go
+++ server/server.go
@@ -171,7 +171,7 @@ func initDatabase(logger *logrus.Logger) database.Database {
 	}
 
 	var psqlInfo string
-	if sslkeyPath != "" && sslcertPath != "" && sslcaPath != "" {
+	if sslkeyPath != "" && sslcertPath != "" && sslcaPath == "" {
 		sslmode = "verify-full"
 		psqlInfo = fmt.Sprintf(
 			"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s sslrootcert=%s sslkey=%s sslcert=%s",
